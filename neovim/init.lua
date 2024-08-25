4e02ea8e728df44b8a605a7d8dc18d58599d7c28
askthedragon <langamer@gmail.com>
2024-08-25 15:47:53 -0500

    adding hyprland conf

--- neovim/init.lua
+++ neovim/init.lua
@@ -413,8 +413,8 @@ require("lazy").setup({
                 group = vim.api.nvim_create_augroup("UserLspConfig", {}),
                 callback = function(args)
                     local client = vim.lsp.get_client_by_id(args.data.client_id)
-                    if client.server_capabilities.inlayHintProvider then
-                        vim.lsp.inlay_hint.enable(args.buf, true)
+                    if client and client.server_capabilities and client.server_capabilities.inlayHintProvider then
+                        vim.lsp.inlay_hint.enable(true, nil)
                     end
                     -- whatever other lsp config you want
                 end,
