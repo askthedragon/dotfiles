8a94d9f44acbd243b281e6f78f1d41280327181b
Jason M. <askthedragon@users.noreply.github.com>
2025-03-29 23:45:28 -0500

    add diagnostic virtual lines

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -102,6 +102,8 @@ vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 -- vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 -- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.diagnostic.config({ virtual_lines = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
